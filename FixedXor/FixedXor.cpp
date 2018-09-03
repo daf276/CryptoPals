662abc947cd038f7be635e553d4fc53bb69c63e1
Atienza Fernandez <david.atienza.fernandez@sap.com>
2018-09-03 14:06:57 +0200

    Starded Fixed Xor

---
+++ FixedXor/FixedXor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by D064693 on 03.09.2018.
+//
+
+#include "FixedXor.h"
---
+++ FixedXor/FixedXor.h
@@ -0,0 +1,16 @@
+//
+// Created by D064693 on 03.09.2018.
+//
+
+#ifndef FIXEDXOR_FIXEDXOR_H
+#define FIXEDXOR_FIXEDXOR_H
+
+
+
+class FixedXor {
+
+};
+
+
+
+#endif //FIXEDXOR_FIXEDXOR_H
